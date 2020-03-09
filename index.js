7705bb119e55455c4192c90612e924e277e099db
Kyra Mellema <kemellema@gmail.com>
2020-03-09 14:23:24 -0400

    finished countdown and countup timers and fixed some CSS for the background images

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const release = new Date("March 20, 2020 10:00:00").getTime();
+const now = new Date().getTime();
 
-const x = setInterval(function() {
-	const now = new Date().getTime();
-	const t = release - now;
-	const days = Math.floor(t / (1000 * 60 * 60 * 24));
-	const hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-	const seconds = Math.floor((t % (1000 * 60)) / 1000);
-	document.getElementById("day").innerHTML = days;
-	document.getElementById("hour").innerHTML = hours;
-	document.getElementById("minute").innerHTML = minutes;
-	document.getElementById("second").innerHTML = seconds;
-}, 1000);
-
-// TODO When the game comes out, switch to count up
+if (now <= release) {
+	const x = setInterval(function() {
+		const now = new Date().getTime();
+		const t = release - now;
+		const days = Math.floor(t / (1000 * 60 * 60 * 24));
+		const hours = Math.floor(
+			(t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((t % (1000 * 60)) / 1000);
+		document.getElementById("day").innerHTML = days;
+		document.getElementById("hour").innerHTML = hours;
+		document.getElementById("minute").innerHTML = minutes;
+		document.getElementById("second").innerHTML = seconds;
+	}, 1000);
+} else {
+	const x = setInterval(function() {
+		const now = new Date().getTime();
+		const t = now - release;
+		const days = Math.floor(t / (1000 * 60 * 60 * 24));
+		const hours = Math.floor(
+			(t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+		);
+		const minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+		const seconds = Math.floor((t % (1000 * 60)) / 1000);
+		document.getElementById("day").innerHTML = days;
+		document.getElementById("hour").innerHTML = hours;
+		document.getElementById("minute").innerHTML = minutes;
+		document.getElementById("second").innerHTML = seconds;
+	}, 1000);
+}
